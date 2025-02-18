22d04789da9cf228357d61b75d284f5422af19f8
André Melchior <andre.melchior@outlook.com>
2025-02-17 23:27:13 -0300

    feat: add simple loop with 'while'

---
+++ while.c
@@ -0,0 +1,14 @@
+//loop while
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+        int cont = 0;
+
+        while (cont <= 10){
+                printf("%d \n", cont);
+                cont++;
+        }
+        return 0;
+}
