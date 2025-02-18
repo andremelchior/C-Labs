535814c8d28f84cb89058cf32c4fba6053c48250
André Melchior <andre.melchior@outlook.com>
2025-02-17 23:27:35 -0300

    feat: add simple loop with 'do..while'

---
+++ dowhile.c
@@ -0,0 +1,13 @@
+//loop do..while
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+        int i = 0;
+        do{
+                printf("%d \n", i);
+                i = i + 1;
+        }while(i<11);
+        return 0;
+}
